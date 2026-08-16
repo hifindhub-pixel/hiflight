dc22df12be78bd6b837ef25ee49d7833b48442f2
hifindhub-pixel <hifindhub@gmail.com>
2026-08-17 00:49:42 +0200

    Replace service icons with premium vector set

--- HIFLIGHT-Phase-2/components/ServiceTabs.tsx
+++ HIFLIGHT-Phase-2/components/ServiceTabs.tsx
@@ -11,18 +11,40 @@ const services = [
 
 function ServiceIcon({ id }: { id: Props["active"] }) {
   if (id === "flights") {
-    return <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M3.5 14.5 10 12l-4-7 2-1 6 6 5.5-2c1.4-.5 2.5.1 2.8.9.3.9-.2 2-1.6 2.7L15 14l-2 8-2 .5v-7l-5.5 2.2-2-3.2Z" /></svg>;
+    return (
+      <svg viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M3 20h18" className="service-icon-detail" />
+        <path d="m3.7 14.7 2.9.8 3.2-1.6-3.4-7.6 1.8-.9 5.4 6.4 4.6-2.3c1.4-.7 2.7-.3 3.2.7.5 1-.1 2.2-1.5 2.9L8.7 18.7a3 3 0 0 1-2.2.2l-2.1-.6-1.7-2.8 1-.8Z" />
+      </svg>
+    );
   }
 
   if (id === "hotels") {
-    return <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M3 19v-9m18 9v-7a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v3m-4 0h18M7 10V7.8C7 6.8 6.2 6 5.2 6h-.4C3.8 6 3 6.8 3 7.8V10" /></svg>;
+    return (
+      <svg viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M3 20V8.5A2.5 2.5 0 0 1 5.5 6h13A2.5 2.5 0 0 1 21 8.5V20" />
+        <path d="M3 11h18M3 17h18" />
+        <path d="M7 11V9h3v2m4 0V9h3v2" className="service-icon-detail" />
+      </svg>
+    );
   }
 
   if (id === "cars") {
-    return <svg viewBox="0 0 24 24" aria-hidden="true"><path d="m5 16-1.5-1.5V11l2-5h13l2 5v3.5L19 16M3.5 11h17M7 14h.01M17 14h.01M6 16v2m12-2v2" /></svg>;
+    return (
+      <svg viewBox="0 0 24 24" aria-hidden="true">
+        <path d="m5.2 10 1.5-4h10.6l1.5 4" />
+        <path d="M4.5 10h15A1.5 1.5 0 0 1 21 11.5v5A1.5 1.5 0 0 1 19.5 18h-15A1.5 1.5 0 0 1 3 16.5v-5A1.5 1.5 0 0 1 4.5 10Z" />
+        <path d="M6 18v2m12-2v2M7 14h.01M17 14h.01" className="service-icon-detail" />
+      </svg>
+    );
   }
 
-  return <svg viewBox="0 0 24 24" aria-hidden="true"><path d="M6 17V6.5C6 4.6 8.7 3 12 3s6 1.6 6 3.5V17m-12 0h12M8 20l2-3m6 3-2-3M8.5 7h7M9 13h.01M15 13h.01" /></svg>;
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M6 17V6.5C6 4.6 8.7 3 12 3s6 1.6 6 3.5V17a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2Z" />
+      <path d="M6 10h12M9 19l-2 3m8-3 2 3M9 15h.01M15 15h.01" className="service-icon-detail" />
+    </svg>
+  );
 }
 
 export default function ServiceTabs({ active }: Props) {
