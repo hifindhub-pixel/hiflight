418c4e17d68e6d382aed7ce5cce0246b66483307
hifindhub-pixel <hifindhub@gmail.com>
2026-08-24 02:28:26 +0200

    Add Impact site verification token

--- HIFLIGHT-Phase-2/app/layout.tsx
+++ HIFLIGHT-Phase-2/app/layout.tsx
@@ -15,6 +15,37 @@ export const metadata: Metadata = {
   twitter: { card: "summary_large_image", images: ["/hiflight-hero.jpg"] }
 };
 
+const impactTrackingScript = `(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7671073-7827-44f2-ad25-a30f19bbfa731.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`;
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="fr"><head><script type="text/javascript" dangerouslySetInnerHTML={{ __html: "(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7671073-7827-44f2-ad25-a30f19bbfa731.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');" }} /></head><body><Analytics /><Header />{children}<Footer /><CookieBanner /></body></html>;
+  return (
+    <html lang="fr">
+      <head>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: impactTrackingScript }} />
+      </head>
+      <body>
+        <span
+          aria-hidden="true"
+          style={{
+            position: "absolute",
+            width: 1,
+            height: 1,
+            padding: 0,
+            margin: -1,
+            overflow: "hidden",
+            clip: "rect(0, 0, 0, 0)",
+            whiteSpace: "nowrap",
+            border: 0,
+          }}
+        >
+          Impact-Site-Verification: 52b1d8cb-a0bb-4e31-b350-02e35cb25e61
+        </span>
+        <Analytics />
+        <Header />
+        {children}
+        <Footer />
+        <CookieBanner />
+      </body>
+    </html>
+  );
 }
