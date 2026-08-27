8742cc07701f6a64c463726b4b7e1f01edfdea0f
hifindhub-pixel <hifindhub@gmail.com>
2026-08-27 23:03:01 +0200

    Gate affiliate and review scripts behind consent

--- HIFLIGHT-Phase-2/components/AffiliateScripts.tsx
+++ HIFLIGHT-Phase-2/components/AffiliateScripts.tsx
@@ -16,5 +16,14 @@ export default function AffiliateScripts() {
   }, []);
 
   if (!allowed) return null;
-  return <Script id="cj-deep-link-automation" src="https://www.anrdoezrs.net/am/101723457/impressions/page/am.js" strategy="afterInteractive" />;
+
+  return (
+    <>
+      <Script id="impact-affiliate-tracking" strategy="afterInteractive">
+        {"(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7671073-7827-44f2-ad25-a30f19bbfa731.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');"}
+      </Script>
+      <Script id="cj-deep-link-automation" src="https://www.anrdoezrs.net/am/101723457/impressions/page/am.js" strategy="afterInteractive" />
+      <Script id="trustpilot-widget" src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" strategy="afterInteractive" />
+    </>
+  );
 }
